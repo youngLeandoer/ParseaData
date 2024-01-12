32127817da71ea996b96d69af01388ca2f5835c9
Mathew <liroyjen2@gmail.com>
2024-01-12 17:41:55 +0300

    add init.sql

---
+++ Diploma/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE imported_data (
+                               id SERIAL PRIMARY KEY,
+                               column1 VARCHAR(255),
+                               column2 VARCHAR(255),
+                               column3 INTEGER,
+                               column4 DATE
+);
